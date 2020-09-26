cc5ab21eb033c622b5c76926ed8b2fb6ba351360
dmaiocchi <dmaiocchi@suse.com>
2020-09-26 13:58:47 +0200

    Add mutex for blocking concurrent hana handler
    This is needed to prevent to run multiple times crm move resource

--- main.go
+++ main.go
@@ -7,7 +7,11 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-const handlerWebSrvPort = "9999"
+const (
+	handlerWebSrvPort  = "9999"
+	sapHandlerName     = "/hooks-sap"
+	defautlHandlerName = "/hooks-default"
+)
 
 // default handler. this is where the alerts witch doesn't match anything goes
 func defaultHandler(_ http.ResponseWriter, req *http.Request) {
@@ -22,11 +26,15 @@ func defaultHandler(_ http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+
 	log.Infof("starting handler on port: %s", handlerWebSrvPort)
 
 	// register the various handler
-	http.HandleFunc("/hooks-sap", handlerHanaDiskFull)
-	http.HandleFunc("/hooks-default", defaultHandler)
+	h := new(HanaDiskFull)
+	// make sure we run only 1 handler until it finish.
+	http.HandleFunc(sapHandlerName, h.handlerHanaDiskFull)
+
+	http.HandleFunc(defautlHandlerName, defaultHandler)
 
 	// TODO: serve webserver (future https)
 	err := http.ListenAndServe(":"+handlerWebSrvPort, nil)
