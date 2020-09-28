ea4de4eac1737db8a15c80ea3c9858553f04b0a6
dmaiocchi <dmaiocchi@suse.com>
2020-09-28 21:37:51 +0200

    Add config handler for file yaml

---
+++ config.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"os"
+
+	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
+	flag "github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+const binaryName = "hasap-alerts-handler"
+
+// Config new config from yaml
+func Config(flagSet *flag.FlagSet) (*viper.Viper, error) {
+	config := viper.New()
+
+	err := config.BindPFlags(flagSet)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not bind config to CLI flags")
+	}
+
+	// try to get the "config" value from the bound "config" CLI flag
+	path := config.GetString("config")
+	if path != "" {
+		// try to manually load the configuration from the given path
+		err = loadConfigurationFromFile(config, path)
+	} else {
+		// otherwise try viper's auto-discovery
+		err = loadConfigurationAutomatically(config)
+	}
+
+	if err != nil {
+		return nil, errors.Wrap(err, "could not load configuration file")
+	}
+
+	setLogLevel(config.GetString("log-level"))
+
+	return config, nil
+}
+
+func loadConfigurationAutomatically(config *viper.Viper) error {
+	config.SetConfigName(binaryName)
+	config.AddConfigPath("/usr/etc/")
+
+	err := config.ReadInConfig()
+	if err == nil {
+		log.Info("Using config file: ", config.ConfigFileUsed())
+		return nil
+	}
+
+	if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+		log.Infof("Could not discover configuration file: %s", err)
+		log.Info("Default configuration values will be used")
+		return nil
+	}
+
+	return errors.Wrap(err, "could not load automatically discovered config file")
+}
+
+// loads configuration from an explicit file path
+func loadConfigurationFromFile(config *viper.Viper, path string) error {
+	// we hard-code the config type to yaml, otherwise ReadConfig will not load the values
+	// see https://github.com/spf13/viper/issues/316
+	config.SetConfigType("yaml")
+
+	file, err := os.Open(path)
+	if err != nil {
+		return errors.Wrap(err, "could not open file")
+	}
+	defer file.Close()
+
+	err = config.ReadConfig(file)
+	if err != nil {
+		return errors.Wrap(err, "could not read file")
+	}
+
+	log.Info("Using config file: ", path)
+
+	return nil
+}
+
+func setLogLevel(level string) {
+	switch level {
+	case "error":
+		log.SetLevel(log.ErrorLevel)
+	case "warn":
+		log.SetLevel(log.WarnLevel)
+	case "info":
+		log.SetLevel(log.InfoLevel)
+	case "debug":
+		log.SetLevel(log.DebugLevel)
+	default:
+		log.Warnln("Unrecognized minimum log level; using 'info' as default")
+	}
+}
--- go.mod
+++ go.mod
@@ -5,11 +5,11 @@ go 1.13
 require (
 	github.com/containerd/containerd v1.4.1
 	github.com/google/martian v2.1.0+incompatible
-	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pkg/errors v0.9.1
 	github.com/shurcooL/httpfs v0.0.0-20190707220628-8d4bc4ba7749
 	github.com/sirupsen/logrus v1.6.0
 	github.com/spf13/pflag v1.0.5
-	github.com/stretchr/testify v1.3.0 // indirect
+	github.com/spf13/viper v1.7.1
 	golang.org/x/tools v0.0.0-20200925191224-5d1fdd8fa346 // indirect
 	google.golang.org/grpc v1.32.0 // indirect
 )
